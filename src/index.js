100235d31ab358a3e7288669058a8f4fe31afcb8
jpdagnano <jeanp.dagnano@gmail.com>
2022-10-01 22:28:54 +0200

    diverses erreurs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Validcreation from "./pages/Validcreation";
