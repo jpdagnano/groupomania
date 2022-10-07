a3ddf0ad870ae685d5026a169ee78ae5d3de2d14
jpdagnano <jeanp.dagnano@gmail.com>
2022-10-08 00:13:57 +0200

    envoi donnees article

--- src/components/Form-create.js
+++ src/components/Form-create.js
@@ -1,24 +1,80 @@
 import "../styles/createpost.css";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(data) {
+    fetch("http://localhost:3001/createpost", {
+      ContentType: "application/x-www-form-urlencoded",
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then(function (response) {
+      if (response.ok) {
+        navigate("/main");
+        console.log("fait");
+      } else {
+        alert("Champ manquant/erreur avec l'image");
+      }
+    });
+  }
+
   return (
-    <form className="bloc-form">
+    <div className="bloc-form">
       <div className="div-full-form">
-        <div className="div-elem-form">
-          <label for="titre">Titre de l'article:</label>
-          <input type="text" className="input-content"></input>
-        </div>
-        <div className="div-elem-form">
-          <label for="description">Article:</label>
-          <input type="text" className="input-content"></input>
-        </div>
-        <div class="form-group">
-          <laber for="image">Insérer une image</laber>
-          <input type="file" accept="image/*" />
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-elem">
+            <div className="div-elem-form">
+              <label for="titre">Titre de l'article:</label>
+              <input
+                type="text"
+                autocomplete="nope"
+                {...register("titre", {
+                  required: true,
+                  minLength: 10,
+                })}
+              />
+              {errors.title && (
+                <p>Veuillez saisir un titre de 10 caractères minimun</p>
+              )}
+            </div>
+            <div className="div-elem-form">
+              <label for="description">Article:</label>
+              <input
+                type="text"
+                autocomplete="nope"
+                {...register("description", {
+                  required: true,
+                  minLength: 50,
+                })}
+              />
+              {errors.article && (
+                <p>Veuillez saisir un article de 50 caractères minimun</p>
+              )}
+            </div>
+            <div class="form-group">
+              <label for="imageUrl">Insérer une image</label>
+              <input
+                type="file"
+                autocomplete="nope"
+                {...register("imageUrl", {
+                  required: true,
+                })}
+              />
+            </div>
+            <div className="action">
+              <button className="login-btn">Envoyer</button>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
-
 export default CreatePost;
