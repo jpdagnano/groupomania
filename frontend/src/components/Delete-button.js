26fc0a51b3b73b3ceecbb11ac0dfbdf39b09f88b
jpdagnano <jeanp.dagnano@gmail.com>
2022-11-11 18:31:54 +0100

    deletebutton

--- frontend/src/components/Delete-button.js
+++ frontend/src/components/Delete-button.js
@@ -5,11 +5,19 @@ const longId = new URLSearchParams(pageIdWindow);
 const pageId = longId.get("_id");
 
 function DeleteFetch() {
+  const navigate = useNavigate();
   fetch("http://localhost:3001/updatepost?_id=" + pageId, {
     method: "DELETE",
     headers: new Headers({
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     }),
+  }).then(function (response) {
+    if (response.ok) {
+      navigate("/main");
+      console.log("fait");
+    } else {
+      alert("erreur");
+    }
   });
 }
 
